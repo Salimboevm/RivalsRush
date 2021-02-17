acfba5da1ac17f859b7b3062c7ab87cd5e78d4c6
Salimboevm <salimboyevm@gmail.com>
2021-02-17 15:46:18 +0500

    a

--- Assets/Scripts/playercontroller.cs
+++ Assets/Scripts/playercontroller.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontroller : MonoBehaviour
+public class playercontroller : MonoBehaviourPun
 {
     public float movespeed = 2; //defines the variable for move speed
     public float forcejump = 2; //defines the variable for the force of a jump
@@ -18,22 +19,26 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movedirect = Input.GetAxis("Horizontal"); //enables movement on the horizontal axis 
-        transform.position += new Vector3(movedirect, 0,0) * Time.deltaTime * movespeed;
+        if (base.photonView.IsMine)//check who is owner
+        {
+            movedirect = Input.GetAxis("Horizontal"); //enables movement on the horizontal axis 
+            transform.position += new Vector3(movedirect, 0, 0) * Time.deltaTime * movespeed;
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) <0.001f)//if the button is down the jump will initiate
-        {
-            _rigidbody.AddForce(new Vector2(0, forcejump), ForceMode2D.Impulse); //enabled jumping
+            if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)//if the button is down the jump will initiate
+            {
+                _rigidbody.AddForce(new Vector2(0, forcejump), ForceMode2D.Impulse); //enabled jumping
+            }
+
+            if (movedirect > 0 && !right)//if facing left then player character flips right
+            {
+                playerflip();
+            }
+            else if (movedirect < 0 && right)//if facing right player flips left
+            {
+                playerflip();
+            }
         }
 
-        if (movedirect > 0 && !right)//if facing left then player character flips right
-        {
-            playerflip();
-        }
-        else if (movedirect < 0 && right)//if facing right player flips left
-        {
-            playerflip();
-        }
     }
     void playerflip() //method to flip the player to face the other way
     {
