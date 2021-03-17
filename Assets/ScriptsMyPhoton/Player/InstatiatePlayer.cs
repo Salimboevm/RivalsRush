8298e859949378fa97968357274f7834fd365f39
Salimboevm <salimboyevm@gmail.com>
2021-03-17 22:21:44 +0500

    Revert "Revert "r""
    This reverts commit e2a93916acf4376b3bc2f5c7812a4567d1cca116.

--- Assets/ScriptsMyPhoton/Player/InstatiatePlayer.cs
+++ Assets/ScriptsMyPhoton/Player/InstatiatePlayer.cs
@@ -6,14 +6,15 @@ public class InstatiatePlayer : MonoBehaviour
 {
     [SerializeField]
     private GameObject playerPrefab;
-    GameObject temp;
-
-    public GameObject Temp { get => temp; }
 
     private void Awake()
     {
         Vector2 offset = Random.insideUnitCircle * 3f;
         Vector3 pos = new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z);
+<<<<<<< HEAD
         Master.NetworkInstantiate(playerPrefab, gameObject.transform.position, Quaternion.identity);
+=======
+        Master.NetworkInstantiate(playerPrefab, pos, Quaternion.identity);
+>>>>>>> parent of f024b86 (camera following)
     }
 }
