282abc70ddd5e8d77ffc17bc6aec70f576a7c470
Salimboevm <salimboyevm@gmail.com>
2021-03-22 19:10:07 +0500

    working

--- Assets/ScriptsMyPhoton/Player/playercontroller.cs
+++ Assets/ScriptsMyPhoton/Player/playercontroller.cs
@@ -3,13 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream:Assets/ScriptsMyPhoton/Player/playercontroller.cs
 public class playercontroller : MonoBehaviourPun, IPunObservable
+=======
+public class PlayerController : MonoBehaviourPun
+>>>>>>> Stashed changes:Assets/Scripts/playercontroller.cs
 {
     public float movespeed = 2; //defines the variable for move speed
     public float forcejump = 2; //defines the variable for the force of a jump
     private float movedirect; //what direction player will move
     private bool right = true; //player is facing right is true
-  
+    
     //[SerializeField]
     private Rigidbody2D _rigidbody; //references the rigidbody
     void Start()
@@ -46,7 +50,8 @@ public class playercontroller : MonoBehaviourPun, IPunObservable
     void playerflip() //method to flip the player to face the other way
     {
         right = !right; //inverse
-        _rigidbody.transform.Rotate(0f, 180f, 0f); //rotate 180 on the y axis
+        _rigidbody.transform.Rotate(0f, 180f, 0f, 0f); //rotate 180 on the y axis
+
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
